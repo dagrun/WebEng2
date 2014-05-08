1c987f88fe65f2a770298e6d05fe9165fab34927
dagrun <dagrunh@gmail.com>
2014-05-08 15:23:58 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MeetAct::Application.config.secret_key_base = '2a772cdfbd34fd703ffb778c978d1fb09a1832012c7ebccb5f100a7fd1d29b7d2c3c99513d8871a61324ec83e0e0b3fc6be0150bc0084d423ae2edebf95b1df6'
