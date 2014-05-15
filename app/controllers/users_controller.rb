406abae57c312b667097d7e5e587b11f6de3f720
dagrun <dagrunh@gmail.com>
2014-05-15 17:33:11 +0200

    made it so that the view for current user is dirrent tahn at other users. can create new group in the view of itself, but not in other users view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @groups = @user.groups
-    if @group.nil?
-      @group = Group.new
+    if @user == current_user
+        @group = Group.new
     end
   end
 
@@ -60,8 +60,6 @@ class UsersController < ApplicationController
     def set_user
       @user = User.find(params[:id])
     end
-
-    private
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
